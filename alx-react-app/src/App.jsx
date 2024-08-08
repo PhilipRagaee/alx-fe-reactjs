64c0faa1f301f3e279af788a0e29c0c3de806ebb
PHILIP-RAGAEE <philip.ragaee@ipg-eg.com>
2024-08-08 11:10:08 +0300

    Editing App.jsx File

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import WelcomeMessage from './components/WelcomeMessage';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <div>
+        <WelcomeMessage/>
+      </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,9 +32,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <div>
-        <WelcomeMessage/>
-      </div>
     </>
   )
 }
