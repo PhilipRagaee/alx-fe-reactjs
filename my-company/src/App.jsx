4d81a895da9a01fe02fc640d2a9c202b0c41826b
PHILIP-RAGAEE <philip.ragaee@ipg-eg.com>
2024-08-16 10:02:15 +0300

    Adding vite To JSON

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
