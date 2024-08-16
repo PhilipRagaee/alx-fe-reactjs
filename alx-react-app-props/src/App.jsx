5e983cc13aa64a077d2746c7af05e77d1127d1f4
PHILIP-RAGAEE <philip.ragaee@ipg-eg.com>
2024-08-16 08:51:17 +0300

    Editing UserContext

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import UserContext from './components/UserContext';
 
 // ...rest of the code
 
