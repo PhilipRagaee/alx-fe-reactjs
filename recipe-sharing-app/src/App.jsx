8a979dba0e2080efd4cee36900757c96a1b33d16
PHILIP-RAGAEE <philip.ragaee@ipg-eg.com>
2024-08-21 15:51:40 +0300

    Modifing App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'AddRecipeForm' from '/src/components/AddRecipeForm'
+import 'RecipeList' from '/src/components/RecipeList'
 
 function App() {
   const [count, setCount] = useState(0)
