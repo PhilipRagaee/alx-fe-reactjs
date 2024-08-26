a2781d4ce3afc4f329603679abe2899c20f5a3d7
Philip Ragaee <philip.ragaee@ipg-eg.com>
2024-08-26 07:12:04 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -19,6 +19,22 @@ const useRecipeStore = create((set) => ({
     filteredRecipes: recipes,
   }),
 
+  // Update an existing recipe
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+    filteredRecipes: state.filteredRecipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+  })),
+
+  // Delete a recipe
+  deleteRecipe: (recipeId) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+    filteredRecipes: state.filteredRecipes.filter((recipe) => recipe.id !== recipeId),
+  })),
+
   // Set the search term for filtering recipes
   setSearchTerm: (term) => set((state) => {
     const filteredRecipes = state.recipes.filter(recipe =>
