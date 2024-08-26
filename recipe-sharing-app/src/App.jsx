93a465fbcad31ab2d06ca70937ed41197b89ec8b
Philip Ragaee <philip.ragaee@ipg-eg.com>
2024-08-26 07:14:57 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
@@ -7,13 +8,17 @@ import AddRecipeForm from './components/AddRecipeForm';
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <SearchBar />
-      <RecipeList />
-      <FavoritesList />
-      <RecommendationsList />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add-recipe" element={<AddRecipeForm />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+          <Route path="/edit-recipe/:id" element={<EditRecipeForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
