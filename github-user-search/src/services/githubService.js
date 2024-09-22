676a2f2fb8c5da0a8c915769710c33e8439ffce3
PHILIP-RAGAEE <philip.ragaee@ipg-eg.com>
2024-09-22 10:37:15 +0300

    --

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
-export const fetchAdvancedUserData = async (username, location, repos) => {
-  const query = `q=${username} ${location ? `location:${location}` : ''} ${
-    repos ? `repos:>${repos}` : ''
-  }`;
-  const url = `https://api.github.com/search/users?${query}`;
+export const fetchAdvancedUserData = async (username, location, minRepos) => {
+  let query = `q=${username ? `${username}` : ''}`;
   
+  if (location) {
+    query += ` location:${location}`;
+  }
+
+  if (minRepos) {
+    query += ` repos:>=${minRepos}`;
+  }
+
+  const url = `https://api.github.com/search/users?${query}`;
+
   const response = await axios.get(url);
+  
   return response.data;
 };
